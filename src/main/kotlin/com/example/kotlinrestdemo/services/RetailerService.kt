e093c8c0acec74091fb3b22e4a382be0bcc3fe0a
staticsound <johngreedjr@gmail.com>
2021-04-21 22:41:52 -0700

    minor refactor

--- src/main/kotlin/com/example/kotlinrestdemo/services/RetailerService.kt
+++ src/main/kotlin/com/example/kotlinrestdemo/services/RetailerService.kt
@@ -2,27 +2,26 @@ package com.example.kotlinrestdemo.services
 
 import com.example.kotlinrestdemo.models.Retailer
 import com.example.kotlinrestdemo.repositories.RetailerRepository
-import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 
 @Service
-class RetailerService(val db: RetailerRepository) {
-    fun findRetailers(): List<Retailer> = db.findAll()
+class RetailerService(val retailerRepository: RetailerRepository) {
+    fun findRetailers(): List<Retailer> = retailerRepository.findAll()
 
     fun postRetailer(name: String, stock: Int): String{
         val retailer = Retailer()
         retailer.retailerName = name
         retailer.retailerStock = stock
 
-        db.save(retailer)
+        retailerRepository.save(retailer)
         return "$name has been added with a stock of $stock."
     }
 
     fun deleteRetailer(name: String): String {
-        val entityToDelete = db.findByRetailerName(name)
+        val entityToDelete = retailerRepository.findByRetailerName(name)
 
         if (entityToDelete != null) {
-            db.delete(entityToDelete)
+            retailerRepository.delete(entityToDelete)
         }
 
         return "$name has been deleted."
@@ -33,15 +32,15 @@ class RetailerService(val db: RetailerRepository) {
             return "A value of $stock is not allowed."
         }
 
-        val entityToUpdate = db.findByRetailerName(name)
+        val entityToUpdate = retailerRepository.findByRetailerName(name)
 
         if (entityToUpdate != null) {
             entityToUpdate.retailerStock = stock
-            db.save(entityToUpdate)
+            retailerRepository.save(entityToUpdate)
             return "$name updated stock to $stock"
         }
         return "Name cannot be null"
     }
 
-    fun getHighestStock() = db.findTopByOrderByRetailerStockDesc()
+    fun getHighestStock() = retailerRepository.findTopByOrderByRetailerStockDesc()
 }
